07e0c62395734343c0e2fdf022d5eb26c0d29264
rjha27404 <rjha27404@gmail.com>
2022-10-03 17:57:58 +0530

    Initial Commit

---
+++ src/main/java/com/jenkins/continuousintegrationjenkins/ContinuousIntegrationJenkinsApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.continuousintegrationjenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinuousIntegrationJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinuousIntegrationJenkinsApplication.class, args);
+	}
+
+}
